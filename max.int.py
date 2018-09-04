52a3e20f7fe205344a4cdfd93b618e2b57b6892c
Aríel Jóhann Árnason <ariel17@ru.is>
2018-09-04 10:00:32 +0000

    setti array

--- max.int.py
+++ max.int.py
@@ -2,14 +2,16 @@ num_int = int(input("Input a number: "))    # Do not change this line
 
 while 1:
     num_int2 = int(input("Input another number: "))
-    numbers = [num_int, num_int2]
+    numbers.append(num_int2)
+    numbers = [num_int, 0]
     if num_int2 < 0:
      break
 
-max_int = max(numbers)
+    max_int = max(numbers)
 
 
 print("The maximum is", max_int)    # Do not change this line
 
 #Design an algorithm that finds the maximum positive integer input by a user.  
 #The user repeatedly inputs numbers until a negative value is entered.
+#
