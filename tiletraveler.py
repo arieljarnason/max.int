8606fc79c3eeef3cc3befb5034bbf8e3d4cd7613
Aríel Jóhann Árnason <ariel17@ru.is>
2018-09-18 09:15:46 +0000

    Byrjum að skrifa upp algorithm og hugmyndir um hvernig forrit gæti litið út

--- tiletraveler.py
+++ tiletraveler.py
@@ -2,3 +2,10 @@
     if( x og y) þá hvað: m.v. aðstæður á hverjum stað fyrir sig
     þegar character nær inn í 3.1 þá victory
     í hverri loopu skrifast út status update, you can travel: 
+
+    """
+
+#FASTAR
+
+
+char_x, char_y = 1, 1
